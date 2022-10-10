4a4206285f0c6256ac67249f16bd2b0f3e3cef90
Joshua <bubbahamby@gmail.com>
2022-10-10 10:18:50 -0600

    Updated CMake with correct stack space size option

--- Assignment4/sortutils.cpp
+++ Assignment4/sortutils.cpp
@@ -1,17 +1,17 @@
 #include "sortutils.hpp"
 
-void initializeRawArrayFromStdArray(const SourceArray& source, int dest[]){
-
+void initializeRawArrayFromStdArray(const SourceArray& source, int dest[])
+{
 }
-void organPipeStdArray(SourceArray& data){
-
+void organPipeStdArray(SourceArray& data)
+{
 }
-void evaluateRawArray(const SourceArray& random, const SourceArray& sorted, const SourceArray& reversed, const SourceArray& organPipe, const SourceArray& rotated){
-
+void evaluateRawArray(const SourceArray& random, const SourceArray& sorted, const SourceArray& reversed, const SourceArray& organPipe, const SourceArray& rotated)
+{
 }
-void evaluateStdArray(const SourceArray& random, const SourceArray& sorted, const SourceArray& reversed, const SourceArray& organPipe, const SourceArray& rotated){
-
+void evaluateStdArray(const SourceArray& random, const SourceArray& sorted, const SourceArray& reversed, const SourceArray& organPipe, const SourceArray& rotated)
+{
 }
-void evaluateStdVector(const SourceArray& random, const SourceArray& sorted, const SourceArray& reversed, const SourceArray& organPipe, const SourceArray& rotated){
-
+void evaluateStdVector(const SourceArray& random, const SourceArray& sorted, const SourceArray& reversed, const SourceArray& organPipe, const SourceArray& rotated)
+{
 }
