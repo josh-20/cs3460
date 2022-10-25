daadf91e8d71b63bf84b04f15a92e7d654adccaa
Joshua <bubbahamby@gmail.com>
2022-10-25 08:59:52 -0600

    word Tree created

--- Assignment5/WordTree.cpp
+++ Assignment5/WordTree.cpp
@@ -1,28 +1,52 @@
 #include "WordTree.hpp"
 #include <iostream>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <fstream>
 
 
-WordTree::WordTree(std::string letter, WordTree* parent, std::vector<WordTree*> children, bool end){
-    m_letter = letter;
-    m_parent = parent;
-    m_children = children;
-    m_end = end;
+WordTree::WordTree(){
+}
+void WordTree::add(std::string word){
+    for (char i : word){
+
+        
+    }
+
+}
+bool WordTree::find(){
+
+}
+std::vector<std::string> WordTree::predict(){
+
+}
+std::size_t WordTree::size(){
+
 }
 
-std::string getLetter(){
-    return m_letter;
+std::shared_ptr<WordTree> readDictionary(std::string filename)
+{
+    auto wordTree = std::make_shared<WordTree>();
+    std::ifstream inFile = std::ifstream(filename, std::ios::in);
+
+    while (!inFile.eof())
+    {
+        std::string word;
+        std::getline(inFile, word);
+        // Need to consume the carriage return character for some systems, if it exists
+        if (!word.empty() && word[word.size() - 1] == '\r')
+        {
+            word.erase(word.end() - 1);
+        }
+        // Keep only if everything is an alphabetic character -- Have to send isalpha an unsigned char or
+        // it will throw exception on negative values; e.g., characters with accent marks.
+        if (std::all_of(word.begin(), word.end(), [](unsigned char c) { return std::isalpha(c); }))
+        {
+            std::transform(word.begin(), word.end(), word.begin(), [](char c) { return static_cast<char>(std::tolower(c)); });
+            wordTree->add(word);
+        }
+    }
+
+    return wordTree;
 }
-WordTree getParent(){
-    return m_parent;
-}
-
-std::vector<WordTree> getChildren(){
-    
-}
-
-
-
-
-
-
-
--- Assignment5/WordTree.hpp
+++ Assignment5/WordTree.hpp
@@ -1,32 +1,18 @@
 #pragma once
-#include <string>
-#include <iostream>
 #include <vector>
-#include <stdint.h>
 
-
+struct TreeNode{
+    bool m_endOfWord = false;
+    std::array<std::shared_ptr<TreeNode>,26> m_children;
+};
 
 class WordTree{
     public:
-        WordTree(std::string letter, WordTree* parent, std::vector<WordTree*> children, bool end);
-        WordTree* getParent();
-        std::vector<WordTree*> getChildren();
-        std::string getLetter();
-
+        void add(std::string);
+        bool find();
+        std::vector<std::string> predict();
+        std::size_t size();
     private:
-        std::string m_letter;
-        WordTree* m_parent;
-        std::vector<WordTree*> m_children;
-        bool m_end;
-
-
-
-        
-}
-
-add(std::string word);
-bool find(std::string word);
-std::vector<std::string> predict(std::string partial, std::uint8_t howMany);
-std::size_t size();
-
-
+        std::shared_ptr<TreeNode> m_root = std::make_shared<TreeNode>(TreeNode());
+              
+};
