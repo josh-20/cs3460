eed4429553280c125602226b91c36e14a40f78fb
Joshua <bubbahamby@gmail.com>
2022-11-21 20:27:21 -0700

    class and constructors/destructor built

--- Assignment7/shared_ptr.hpp
+++ Assignment7/shared_ptr.hpp
@@ -1,1 +1,22 @@
 pragma once
+
+namespace usu {
+    template <typename T>
+    class shared_ptr {
+        public:
+            //overloaded constructor
+            shared_ptr(T* pointer);
+            // copy constructor
+            shared_ptr(const shared_ptr& pointer);
+            //move constructor
+            shared_ptr(shared_ptr&& pointer);
+            // destructor
+            ~shared_ptr();
+            // normal methods
+            std::uint8_t user_count();
+            pointer* get();
+            private:
+                std::uint8_t referenceCount = 0;
+    };
+
+}
