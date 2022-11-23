9235a3be92cc3c49db04ec12b555d20f322a6669
Joshua Hamby <joshua.hamby@aggiemail.usu.edu>
2022-11-23 12:23:13 -0700

    class fix and methods added

--- Assignment7/shared_ptr.hpp
+++ Assignment7/shared_ptr.hpp
@@ -1,4 +1,4 @@
-pragma once
+#pragma once
 #include <iostream>
 namespace usu {
     template <typename T>
@@ -12,11 +12,42 @@ namespace usu {
             shared_ptr(shared_pointer&& pointer);
             // destructor
             ~shared_ptr();
+            // Operators           
             // normal methods
-            std::uint8_t user_count();
-            pointer* get();
+            shared_ptr<T> make_shared(Args&&... args);
+            std::uint8_t use_count();
+            T* get();
             private:
-                std::uint8_t referenceCount = 0;
+                std::uint16_t m_referenceCount = 0;
+                T m_pointer;
     };
+    template<typename T>
+    shared_ptr<T>::shared_ptr(T* pointer) :
+        m_pointer(pointer)
+        m_referenceCount(1)
+    {
+    }
+    
+    template <typename T, typename... Args>
+    usu::shared_ptr<T> make_shared(Args&&... args)
+    {
+        return shared_ptr<T>(new T(std::forward<Args>(args)...));
+    }
 
-}
+
+    template<typename T>
+    usu::shared_ptr<T> use_count(){
+        return m_referenceCount;
+    }
+
+    template<typename T>
+    usu::shared_ptr<T> get(){
+
+    }
+
+    template <typename T>
+    usu::shared_ptr<T>::shared_ptr(T* pointer){
+    
+    }
+
+};
