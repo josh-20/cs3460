5962fd7e0bf3e873915ca05f027987c5e124191d
Joshua <bubbahamby@gmail.com>
2022-11-28 10:38:26 -0700

    overloaded operators finished

--- Assignment7/shared_ptr.hpp
+++ Assignment7/shared_ptr.hpp
@@ -9,18 +9,18 @@ namespace usu {
             // copy constructor
             shared_ptr(const shared_ptr& ptr);
             //move constructor
-            shared_ptr(shared_pointer&& ptr);
+            shared_ptr(shared_ptr&& ptr);
             // destructor
             ~shared_ptr();
             // Operators 
-            shared_ptr& operator->();
-            shared_ptr& operator*();           
+            T& operator->() {return m_pointer;};
+            T& operator*() {return *m_pointer;};   
+            shared_ptr& operator=(shared_ptr ptr);       
             // normal methods
-
             std::uint16_t use_count();
             T* get();
             private:
-                unsigned int m_referenceCount = 0;
+                unsigned int* m_referenceCount = 0;
                 T* m_pointer;
     };
     template<typename T>
@@ -36,38 +36,59 @@ namespace usu {
     {
         m_pointer = ptr.m_pointer;
         m_referenceCount = ptr.m_referenceCount;
-        m_referenceCount += 1;
+        (*m_referenceCount)++;
     }
     
     // Move constructor
+    template<typename T>
+    shared_ptr<T>::shared_ptr(shared_ptr&& ptr)
+    {
+        m_pointer = ptr.m_pointer;
+        m_referenceCount = ptr.m_referenceCount;
+        ptr.m_pointer = nullptr;
+    }
+
     template <typename T, typename... Args>
     shared_ptr<T> make_shared(Args&&... args)
     {
         return shared_ptr<T>(new T(std::forward<Args>(args)...));
     }
-
     // use count 
     template<typename T>
     usu::shared_ptr<T> use_count(){
-        return m_referenceCount;
+        return *m_referenceCount;
     }
-
     // get 
     template<typename T>
-    usu::shared_ptr<T> get(){
-        return         
+    usu::shared_ptr<T> get()
+    {
+        // returns  pointer to the managed raw pointer
     }
+
+    //copy operator
+    template <typename T>
+    shared_ptr<T>::operator=(shared_ptr ptr){
+        (*m_referenceCount)--;
+        if (*m_referenceCount == 0){
+            ptr.m_pointer = nullptr;
+            ptr.m_referenceCount - nullptr;
+        }
+        m_pointer = ptr.m_pointer;
+        m_referenceCount = ptr.m_referenceCount;
+        (*m_referenceCount)++;
+    }
+    // Destructor
     template<typename T>
     shared_ptr<T>::~shared_ptr(){
-        m_referenceCount -= 1;
-        if(m_referenceCount == 0){
+        (*m_referenceCount)--;
+        if(*m_referenceCount == 0){
             delete m_pointer;
+            delete m_referenceCount;
+
+            m_pointer = nullptr;
+            m_referenceCount = nullptr;
         }
     }
-
-    // Overloaded operators
-
-
 
     template <typename T, unsigned int N>
     shared_ptr<T[]> make_shared_array()
